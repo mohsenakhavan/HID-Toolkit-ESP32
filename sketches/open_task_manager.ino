7c45d1b469c42263f402de4a6e453b118d400a9e
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-01-31 11:43:52 +0330

    Update open_task_manager.ino

--- sketches/open_task_manager.ino
+++ sketches/open_task_manager.ino
@@ -1,18 +1,14 @@
-// HID Payload 11: Open Task Manager
-#include "Arduino.h"
-#include "USB.h"
-#include "USBHIDKeyboard.h"
-
+#include <USBHIDKeyboard.h>
 USBHIDKeyboard Keyboard;
 
 void setup() {
-    USB.begin();
-    Keyboard.begin();
-    delay(2000);
-    Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.press(KEY_LEFT_SHIFT);
-    Keyboard.press(KEY_ESC);
-    Keyboard.releaseAll();
+  Keyboard.begin();
+  delay(3000);
+  // کلیدهای ترکیبی Ctrl+Shift+Esc
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press(KEY_ESC);
+  Keyboard.releaseAll();
 }
 
 void loop() {}
