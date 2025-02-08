1511f97fe5c0fb06ca272f918380ec937592e5df
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-02-08 23:06:24 +0330

    Create Keyboard_Layout_Change.ino

---
+++ sketches/Keyboard_Layout_Change.ino
@@ -0,0 +1,13 @@
+#include <USBHIDKeyboard.h>
+USBHIDKeyboard Keyboard;
+
+void setup() {
+  Keyboard.begin();
+  delay(3000);
+  
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(' ');
+  Keyboard.releaseAll();
+}
+
+void loop() {}
