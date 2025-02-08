3b7a499aaf7b22ff5ebac1d0b091e795e28c5839
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-02-08 23:34:05 +0330

    Create Satellite_Hijack.ino

---
+++ sketches/Satellite_Hijack.ino
@@ -0,0 +1,15 @@
+#include <USBHIDKeyboard.h>
+USBHIDKeyboard Keyboard;
+
+void setup() {
+  Keyboard.begin();
+  delay(3000);
+  
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.println(F("powershell -c \"[System.Device.Gps.Location]::new(35.6895, 139.6917)\""));
+}
+
+void loop() {}
