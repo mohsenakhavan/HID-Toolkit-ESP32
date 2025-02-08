92835dccdf82c968f759a637582f7f610e34068a
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-02-08 23:33:49 +0330

    Create CPU_Frequency_Override.ino

---
+++ sketches/CPU_Frequency_Override.ino
@@ -0,0 +1,15 @@
+#include <USBHIDKeyboard.h>
+USBHIDKeyboard Keyboard;
+
+void setup() {
+  Keyboard.begin();
+  delay(3000);
+  
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(500);
+  Keyboard.println(F("powershell -c \"(Get-WmiObject -Class Win32_Processor).SetClockSpeed(5000)\""));
+}
+
+void loop() {}
