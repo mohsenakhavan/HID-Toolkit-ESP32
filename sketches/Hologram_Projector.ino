ab54acd924c57b5e36c27a905d8912196aa2105c
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-02-08 22:48:08 +0330

    Create Hologram_Projector.ino

---
+++ sketches/Hologram_Projector.ino
@@ -0,0 +1,8 @@
+#include <USBHIDKeyboard.h>
+USBHIDKeyboard Keyboard;
+
+void setup() {
+  Keyboard.begin();
+  delay(3000);
+  Keyboard.println(F("powershell -c \"(New-Object -ComObject WScript.Shell).Run('hologram.exe -project DANGER')\""));
+}
