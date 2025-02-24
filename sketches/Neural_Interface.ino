8f94b2fd73401238199dc768f95ef14a9b10c6a3
mohsen akhavan <77884120+mohsenakhavan@users.noreply.github.com>
2025-02-24 12:23:41 +0330

    Delete sketches/Neural_Interface.ino

--- sketches/Neural_Interface.ino
+++
@@ -1,8 +0,0 @@
-#include <USBHIDKeyboard.h>
-USBHIDKeyboard Keyboard;
-
-void setup() {
-  Keyboard.begin();
-  delay(3000);
-  Keyboard.println(F("powershell -c \"Add-Type -AssemblyName System.Neural; [System.Neural.BrainSensor]::ReadSignals() | Out-File C:\\brain.txt\""));
-}
